77e96f8b3d5fe8eb04a2ff1d0ffade8b103965ab
Moez Firozbhai Narsidani <c0716607@mylambton.ca>
2018-11-13 14:29:37 -0500

    Add project files.

---
+++ menutoggle/Activity1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using System.Threading;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+
+namespace menutoggle
+{
+    [Activity(Label = "ListActivity", Theme = "@style/splashTheme", MainLauncher = true, NoHistory = true)]
+    public class Activity1 : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState,PersistableBundle bundle)
+        {
+            base.OnCreate(savedInstanceState,bundle);
+
+            // Create your application here
+        }
+        public override void OnBackPressed()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task tk = new Task(() => { StartMainActivity(); });
+            tk.Start();
+        }
+
+        async void StartMainActivity()
+        {
+            await Task.Run(() =>
+            {
+                Task.Delay(10000);
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            });
+        }
+
+
+    }
+}
---
+++ menutoggle/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using System.Threading;
+using Android.Widget;
+using supporttoolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace menutoggle
+{
+    [Activity(Label = "@string/app_name")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.content_main);
+            Timer tim = new Timer(obj =>
+            {
+                RunOnUiThread(() =>
+                {
+                    FindViewById<TextView>(Resource.Id.textbox1).Text = $" {DateTime.Now:T}";
+                });
+            }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+        }
+
+
+
+    }
+}
+
